292be886a96df4a8eff5627403daba95a150becf
eumtsspfc <mateus.ramos.martins@escola.pr.gov.br>
2025-07-04 11:35:24 +0000

    mudando fonte e cores

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-    alert('fui clicado!');
+    const texto = document.querySelector('#entrada-de-texto').value
 }
